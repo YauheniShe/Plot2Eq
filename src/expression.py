32f87a873fde5c5c99b24889e336fea4dbe7ca73
Yauheni <evgenijsheshykov@gmail.com>
2026-03-06 01:43:54 +0300

    Полностью переписан генератор случайных выражений. Теперь выражения выбираются равномерно на основе алгоритма Лампле и Хартона

--- src/expression.py
+++ src/expression.py
@@ -2,6 +2,8 @@ import sympy as sp
 import random
 import signal
 import logging
+import typing as tp
+import numpy as np
 
 from contextlib import contextmanager
 
@@ -19,69 +21,221 @@ def time_limit(seconds):
     finally:
         signal.alarm(0)
 
-OPERATORS = {
-    'Add': {'symbol': sp.Add, 'arity': 2, 'weight': 5.0},
-    'Sub': {'symbol': lambda x, y: x - y, 'arity': 2, 'weight': 5.0},
-    'Mul': {'symbol': sp.Mul, 'arity': 2, 'weight': 5.0},
-    'Div': {'symbol': lambda x, y: x / y, 'arity': 2, 'weight': 2.0},
-    'IntPow': {'symbol': sp.Pow, 'arity': 2, 'weight': 1.0},
-    'sin': {'symbol': sp.sin, 'arity': 1, 'weight': 1.0},
-    'cos': {'symbol': sp.cos, 'arity': 1, 'weight': 1.0},
-    'exp': {'symbol': sp.exp, 'arity': 1, 'weight': 1.0},
-    'log': {'symbol': sp.log, 'arity': 1, 'weight': 2.0},
-    'sqrt': {'symbol': sp.sqrt, 'arity': 1, 'weight': 2.0},
-    'sqr':  {'symbol': lambda x: x**2, 'arity': 1, 'weight': 3.0},
-    'abs': {'symbol': sp.Abs, 'arity': 1, 'weight': 4.0},
-    'Neg': {'symbol': lambda x: -x, 'arity': 1, 'weight': 2.0},
-    'GenExp': {'symbol': sp.Pow, 'arity': 2, 'weight': 1.0}
+x = sp.Symbol('x', real=True)
+
+UNARY_OPS = {
+    'sin': {'symbol': sp.sin, 'weight': 4},
+    'cos': {'symbol': sp.cos, 'weight': 4},
+    'exp': {'symbol': sp.exp, 'weight': 4},
+    'ln': {'symbol': sp.log, 'weight': 4},
+    'sqrt': {'symbol': sp.sqrt, 'weight': 4},
+    'abs': {'symbol': sp.Abs, 'weight' : 4},
+    'tan': {'symbol' : sp.tan, 'weight' : 4},
+    'arcsin' : {'symbol' : sp.asin, 'weight' : 1}
 }
 
-VARIABLES = ['x']
-CONSTANT = ['C']
+BINARY_OPS = {
+    'Add': {'symbol': sp.Add, 'weight': 10},
+    'Sub': {'symbol': lambda a, b: a - b, 'weight': 5},
+    'Mul': {'symbol': sp.Mul, 'weight': 10},
+    'Div': {'symbol': lambda a, b: a / b, 'weight': 5},
+    'Pow': {'symbol': sp.Pow, 'weight': 4}
+}
 
 class ExpressionGenerator:
-    def __init__(self, max_depth: int, const_prob: float, leaf_prob: float, timeout: int) -> None:
-        self.max_depth  = max_depth
-        self.const_prob = const_prob
-        self.leaf_prob = leaf_prob
-        self.timeout = timeout 
+    def __init__(self, max_ops: int, timeout: int, unary_ops: dict = UNARY_OPS, binary_ops: dict = BINARY_OPS) -> None:
+        """
+        :param max_ops: Количество операторов (внутренних узлов) в выражении (n в статье).
+        :param unary_ops: Словарь унарных операторов.
+        :param binary_ops: Словарь бинарных операторов.
+        :param leaves: Список всех возможных листьев (переменные + константы), равномерное распределение L.
+        :param timeout: Таймаут для генерации и упрощения выражения.
+        """
+        self.max_ops = max_ops
+        self.timeout = timeout
+        
+        self.unary_ops = unary_ops
+        self.binary_ops = binary_ops
 
-    def _smart_clean(self, expr):
-        expr = expr.evalf()
-        expr = expr.subs(sp.E, sp.E.evalf())
-        expr = expr.subs(sp.pi, sp.pi.evalf())
-        def replace_trivial_floats(node):
-            if node.is_Float:
-                if node == 1.0:
-                    return sp.Integer(1)
-                if node == -1.0:
-                    return sp.Integer(-1)
-                if node == 0.0:
-                    return sp.Integer(0)
-            return node
-        return expr.replace(lambda x: x.is_Float, replace_trivial_floats)
-    
-    def generate_expr(self, depth=None):
-        if depth is None:
-            depth = self.max_depth
+        self.unary_ops_keys = list(unary_ops.keys())
+        self.unary_ops_weights = [op['weight'] for op in unary_ops.values()]
+        self.p1 = sum(self.unary_ops_weights)
+
+        self.binary_ops_keys = list(binary_ops.keys())
+        self.binary_ops_weights = [op['weight'] for op in binary_ops.values()]
+        self.p2 = sum(self.binary_ops_weights)
+
+
+        self.L = 1
+        self.D = [[0] * (max_ops + 1) for _ in range(max_ops + 2)]
+        
+        for e in range(max_ops + 2):
+            self.D[e][0] = self.L ** e
+            
+        for n in range(1, max_ops + 1):
+            for e in range(1, max_ops - n + 2):
+                self.D[e][n] = self.L * self.D[e - 1][n] + self.p1 * self.D[e][n - 1]
+                if e + 1 < max_ops + 2:
+                    self.D[e][n] += self.p2 * self.D[e + 1][n - 1]
+
+    def _sample_leaf(self):
+        if random.random() < 0.8:
+            return x
+        else:
+            return sp.Integer(random.choice([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]))
+        
+    def _skeletonize(self, expr, c_list):
+        def get_c():
+            c = sp.Symbol(f'C_{len(c_list)}', real=True)
+            c_list.append(c)
+            return c
+
+        if expr == x:
+            return sp.Add(sp.Mul(get_c(), x, evaluate=False), get_c(), evaluate=False)
+        
+        if expr.is_Number:
+            return expr
+
+        if expr.is_Add:
+            return sp.Add(*[self._skeletonize(arg, c_list) for arg in expr.args], evaluate=False)
+
+        if expr.is_Mul:
+            return sp.Mul(*[self._skeletonize(arg, c_list) for arg in expr.args], evaluate=False)
+        
+        if expr.is_Pow:
+            is_sqrt = (expr.args[1] == sp.Rational(1, 2))
+
+            base = self._skeletonize(expr.args[0], c_list)
+            if expr.args[1].has(x):
+                expo = self._skeletonize(expr.args[1], c_list)
+            else:
+                expo = expr.args[1]
+
+            pow_expr = sp.Pow(base, expo, evaluate=False)
+
+            if is_sqrt:
+                return sp.Mul(get_c(), pow_expr, evaluate=False)
+            
+            return pow_expr
+        
+        if expr.is_Function:
+            arg = self._skeletonize(expr.args[0], c_list)
+            return sp.Mul(get_c(), expr.func(arg), evaluate=False)
+        
+        return expr
+
+    def _generate_raw(self, fixed_n: int):
+        """
+        Генерирует AST дерево в префиксной нотации (Алгоритм 2 из статьи),
+        и собирает из него SymPy выражение.
+        """
+        e = 1
+        n = fixed_n
+        tree_nodes =[]
+        
+        while n > 0:
+            probs = []
+            choices =[]
+            for k in range(e):
+                if self.p1 > 0:
+                    w1 = (self.L ** k) * self.p1 * self.D[e - k][n - 1]
+                    if w1 > 0:
+                        probs.append(w1)
+                        choices.append((k, 1))
+                        
+                if self.p2 > 0 and (e - k + 1) < len(self.D):
+                    w2 = (self.L ** k) * self.p2 * self.D[e - k + 1][n - 1]
+                    if w2 > 0:
+                        probs.append(w2)
+                        choices.append((k, 2))
+            
+            if sum(probs) == 0:
+                return None 
+                
+            k, arity = random.choices(choices, weights=probs, k=1)[0]
+            
+            for _ in range(k):
+                tree_nodes.append(('leaf', None))
+                
+            if arity == 1:
+                op_name = random.choices(self.unary_ops_keys, weights=self.unary_ops_weights, k=1)[0]
+                tree_nodes.append(('unary', op_name))
+                e = e - k
+            else:
+                op_name = random.choices(self.binary_ops_keys, weights=self.binary_ops_weights, k=1)[0]
+                tree_nodes.append(('binary', op_name))
+                e = e - k + 1
+                
+            n -= 1
+            
+        for _ in range(e):
+            tree_nodes.append(('leaf', None))
+            
+        iterator = iter(tree_nodes)
+        
+        def build_ast():
+            try:
+                token_type, value = next(iterator)
+            except StopIteration:
+                return None
+                
+            if token_type == 'leaf':
+                return self._sample_leaf()
+            
+            elif token_type == 'unary':
+                arg = build_ast()
+                if arg is None: return None
+                func = self.unary_ops[value]['symbol']
+                return func(arg)
+            elif token_type == 'binary':
+                arg1 = build_ast()
+                arg2 = build_ast()
+                if arg1 is None or arg2 is None: return None
+                func = self.binary_ops[value]['symbol']
+                return func(arg1, arg2)
+                
+        return build_ast()
+
+    def generate_expr(self):
+        n = random.randint(1, self.max_ops)
         while True:
             stage = 'INIT'
             expr = None
             try:
                 with time_limit(self.timeout):
                     stage = 'GENERATE'
-                    expr = self._generate_recursive(depth)
+                    expr = self._generate_raw(n)
                     if expr is None:
                         continue
+                        
                     stage = 'SIMPLIFY'
-                    expr = expr.simplify()
-                    if expr.has(sp.I) or expr.has(sp.nan) or expr.has(sp.zoo) or expr.is_constant():
+                    expr = sp.simplify(expr)
+                    
+                    if expr.has(sp.I, sp.nan, sp.zoo, sp.oo, -sp.oo) or expr.is_constant():
                         continue
-                    stage = 'CLEAN'
-                    expr = self._smart_clean(expr)
-                    stage = 'ROUND'
-                    expr = self._round_floats(expr)
-                    return expr
+                        
+                    stage = 'SKELETONIZE'
+                    c_list = []
+                    skeleton_raw = self._skeletonize(expr, c_list)
+
+                    max_c = min(3, len(c_list))
+                    num_random_c = random.randint(0, max_c)
+
+                    chosen_cs = set(random.sample(c_list, num_random_c))
+                    inactive_c_values = {c : sp.Integer(1) for c in c_list if c not in chosen_cs}
+                    
+                    final_skeleton = sp.simplify(skeleton_raw.subs(inactive_c_values))
+
+                    c_values = {}
+
+                    for c in chosen_cs:
+                        val = random.uniform(1, 5)
+                        c_values[c] = val
+                    
+                    expr_instantiated = final_skeleton.subs(c_values)
+
+                    return final_skeleton, expr, expr_instantiated
+                    
             except TimeoutException:
                 try:
                     bad_expr = str(expr) if expr is not None else 'None'
@@ -89,54 +243,11 @@ class ExpressionGenerator:
                     bad_expr = 'Неизвестное выражение (стадия генерации)'
                 logging.error(f"Timeout[{stage}] {self.timeout} секунд. Пропущено выражение: {bad_expr}")
                 continue
-
             except Exception as e:
                 logging.exception("Ошибка при генерации выражения.")
                 continue
 
-    def _generate_recursive(self, depth=None):
-        if depth is None:
-            depth = self.max_depth
-        is_root = (depth == self.max_depth)
-        if not is_root and (random.random() < self.const_prob or depth == 0):
-            return self._generate_leaf()
-        weights = [OPERATORS[name]['weight'] for name in OPERATORS.keys()]
-        op_name = random.choices(list(OPERATORS.keys()), weights=weights, k=1)[0]
-        op_info = OPERATORS[op_name]
-        arity = op_info['arity']
-        symbol = op_info['symbol']
-        if arity == 1:
-            args = self._generate_recursive(depth - 1)
-            return symbol(args)
-        if arity == 2:
-            if op_name == 'IntPow':
-                first_arg = self._generate_recursive(depth - 1)
-                exponents = [-3, -2, -1, 2, 3]
-                second_arg = sp.Integer(random.choice(exponents))
-                args = (first_arg, second_arg)
-                return symbol(*args)
-            if op_name == 'GenExp':
-                val = round(random.uniform(0, 5), 3)
-                first_arg = sp.Float(val)
-                second_arg = self._generate_recursive(depth - 1)
-                args = (first_arg, second_arg)
-                return symbol(*args)
-            first_arg = self._generate_recursive(depth - 1)
-            second_arg = self._generate_recursive(depth - 1)
-            args = (first_arg, second_arg)
-            return symbol(*args)
+generator = ExpressionGenerator(5, 5)
 
-    def _round_floats(self, expr, precision=3):
-        return expr.xreplace({
-            n: round(n, precision) for n in expr.atoms(sp.Float)
-        })
-
-    def _generate_leaf(self):
-        if random.random() < self.leaf_prob:
-            c_type = random.choice(CONSTANT)
-            if c_type == 'C':
-                val = round(random.uniform(-5, 5), 3)
-                return sp.Float(val)
-        else:
-            var = random.choice(VARIABLES)
-            return sp.Symbol(var, real=True)
+for _ in range(100):
+    print(generator.generate_expr()[0])
